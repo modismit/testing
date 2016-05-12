8847c4f4494e2e573dc661afa0f6b6e36182b224
manish <manish@siliconithub.com>
2016-05-12 14:55:42 +0530

    update latets source code

---
+++ GitTest/app/src/main/java/com/gittest/TEstinSecond.java
@@ -0,0 +1,18 @@
+package com.gittest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by android4 on 12/5/16.
+ */
+public class TEstinSecond extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.testing);
+
+
+    }
+}
